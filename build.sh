f3f128423c31fd4101cc75310294e49a614980ee
Cory Maughmer <cory.maughmer@gmail.com>
2019-01-14 16:51:35 -0600

    Annot.json and AnnotTrack.js caused build errors

--- build.sh
+++ build.sh
@@ -5,12 +5,12 @@ cd /apollo/ && \
 	echo '@import url("cpt.css");' >> client/apollo/css/main.css && \
 	cp /apollo/cpt.css client/apollo/css/cpt.css && \
 	./apollo deploy && \
-	cp /apollo/annot.json /apollo/web-app/jbrowse/plugins/WebApollo/json/annot.json && \
-	cp /apollo/annot.json /apollo/jbrowse-download/plugins/WebApollo/json/annot.json && \
-	cp /apollo/annot.json /apollo/client/apollo/json/annot.json && \
-	cp /apollo/AnnotTrack.js /apollo/client/apollo/js/View/Track/AnnotTrack.js && \
-	cp /apollo/AnnotTrack.js /apollo/web-app/jbrowse/plugins/WebApollo/js/View/Track/AnnotTrack.js && \
-	cp /apollo/AnnotTrack.js /apollo/jbrowse-download/plugins/WebApollo/js/View/Track/AnnotTrack.js && \
+	#cp /apollo/annot.json /apollo/web-app/jbrowse/plugins/WebApollo/json/annot.json && \
+	#cp /apollo/annot.json /apollo/jbrowse-download/plugins/WebApollo/json/annot.json && \
+	#cp /apollo/annot.json /apollo/client/apollo/json/annot.json && \
+	#cp /apollo/AnnotTrack.js /apollo/client/apollo/js/View/Track/AnnotTrack.js && \
+	#cp /apollo/AnnotTrack.js /apollo/web-app/jbrowse/plugins/WebApollo/js/View/Track/AnnotTrack.js && \
+	#cp /apollo/AnnotTrack.js /apollo/jbrowse-download/plugins/WebApollo/js/View/Track/AnnotTrack.js && \
 	./apollo clean-all && ./apollo deploy && \
 	# Move to tmp dir
 	cp /apollo/target/apollo*.war /tmp/apollo.war && \
